de64f77f0bb46f7b7111a24743c9c1b776b0b333
Cizz22 <cisatraa@gmail.com>
2023-09-23 00:56:50 +0700

    minor fix

--- backend/api/models.py
+++ backend/api/models.py
@@ -25,7 +25,7 @@ class User(
     password = models.CharField(max_length=255)
     phone = models.CharField(max_length=255)
 
-    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = ['email', 'password']
 
     def __str__(self):
